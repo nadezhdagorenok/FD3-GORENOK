17c79d5164474f90dfa726bbd6bb66203bc58c21
n.gorenok <n.gorenok@gmail.com>
2018-01-12 00:56:22 +0300

    config tsc

---
+++ Scales/app.ts
@@ -0,0 +1,20 @@
+class Scale {
+    products: Array<string>;
+    add()
+    getSumScale()
+    getNameList()
+}
+
+class Products{
+    getScale()
+    getName()
+
+}
+class Apple extends Products{
+
+}
+class Tomato extends Products{
+
+}
+let greenAple:Apple = new Apple('greenApple', '100г');
+let orangeTomato:Tomato = new Tomato('orangeTomato', '80г');
--- filter/filter.js
+++ filter/filter.js
@@ -20,48 +20,63 @@ var SelectBlock = React.createClass({
       freeTextChanged: function(EO) { 
         console.log('InputText: текст свободного ввода изменён - '+ EO.target.value); 
         var value = EO.target.value;
-        this.setState( {freetext: value}, this.textChanged.bind(this, 'фильтр'));
+        this.setState( {freetext: value}, this.textChanged);
         console.log(this.state.freetext);     
       },
       checkboxClicked: function(EO){
       console.log('Checkbox чекнут - ', EO.target.checked);  
       var checked = EO.target.checked;
-      this.setState({checked:checked}, this.textChanged.bind(this, 'сортировка'));       
+      this.setState({checked:checked}, this.textChanged);       
       },
-      textChanged: function(purpose) {                                
-          var searchdata=[];      
-              
-          console.log(this.state.freetext);
-          if(purpose === 'фильтр'){          
-              var needle = this.state.freetext.toLowerCase();  
-              if(!needle){
-                this.setState({data:this.props.products});
-                return;
-              }
-              console.log('needle=  ', needle);
-              console.log('this.state.data перед фильтром =  ', this.state.data);              
-              searchdata = this.state.data.filter(function(item){
-              return item['text'].toString().toLowerCase().indexOf(needle) > -1;
-              
-          });         
-       }
-        
-        else if(purpose === 'сортировка'){            
-            searchdata = this.state.data.slice();
-            if(!this.state.checked){
-              this.setState({data:this.props.products});
-              return;
-            }
-            console.log(searchdata);
-            searchdata.sort(function(a, b){
-            return a.text > b.text ? 1 : -1;
-          });     
-              
-        }  
+
+      textChanged: function() {                                
+        var searchdata=[];            
+            
+        console.log(this.state.freetext);
+
+        if(this.state.freetext){          
+          var needle = this.state.freetext;  
+          if(!needle){
+            this.setState({data:this.props.products});
+            return;
+          }
+          console.log('needle=  ', needle);
+          console.log('this.state.data перед фильтром =  ', this.state.data);    
+                   
+          searchdata=this.state.data.filter(function(item){
+           
+          return item['text'].toString().toLowerCase().indexOf(needle) > -1;
           
-       this.setState({data:searchdata});         
-       console.log('state.data=  ',this.state.data);  
-        },
+      });         
+   }
+   else {
+    searchdata = this.props.products.slice();
+   }
+   
+      
+       if(this.state.checked){            
+        searchdata = this.state.data.slice();
+        if(!this.state.checked){
+          this.setState({data:this.props.products});
+          return;
+        }
+        console.log(searchdata);
+        searchdata.sort(function(a, b){
+        return a.text > b.text ? 1 : -1;
+      });     
+          
+    }
+    // else {
+    //   searchdata = this.props.products.slice();
+    //  }
+
+     this.setState({data:searchdata});         
+     console.log('state.data=  ',this.state.data);  
+      },
+
+
+
+      
 
     
     
@@ -74,7 +89,7 @@ var SelectBlock = React.createClass({
         );
     
         return React.DOM.div( {className:'SelectBlock'}, 
-                 React.DOM.input({type:'checkbox', className:'CheckBox', defaultChecked:this.state.checked, onChange:this.checkboxClicked}),
+                 React.DOM.input({type:'checkbox', className:'CheckBox', defaultChecked:this.state.checked, onClick:this.checkboxClicked}),
                  React.DOM.input({type:'text',name:'typeText',className:'FreeText', placeholder: 'Поиск', defaultValue:this.state.freetext,onChange:this.freeTextChanged}),  
                  React.DOM.select( {className:'SelectProduct', multiple: true, defaultValue:['стол','стул', 'шкаф', 'полка']}, productsCode),              //defaultValue:['стол','стул', 'шкаф', 'полка']},
                );
