f98d622190a834ae8e4d89ada3e05bdbcaa1f584
n.gorenok <n.gorenok@gmail.com>
2018-02-04 20:36:28 +0300

    corrected adding products to LocalStorage

--- Scales3/app.js
+++ Scales3/app.js
@@ -32,15 +32,9 @@ var ScalesStorageEngineArray = /** @class */ (function () {
 var ScalesStorageEngineLocalStorage = /** @class */ (function () {
     function ScalesStorageEngineLocalStorage() {
     }
-    // private index:number; 
-    // constructor(){
-    //     this.index=0;
-    // }
     ScalesStorageEngineLocalStorage.prototype.addItem = function (item) {
         localStorage.setItem(String(localStorage.length), JSON.stringify(item));
-        // this.index++;      
-        //console.log('Добавлен продукт ' + item['name']);  
-        return localStorage.length;
+        return localStorage.length - 1;
     };
     ;
     ScalesStorageEngineLocalStorage.prototype.getItem = function (index) {
@@ -59,7 +53,7 @@ var Scale = /** @class */ (function () {
     }
     Scale.prototype.add = function (product) {
         this.listProducts.push(product);
-        //console.log('Добавлен на весы продукт ', product['name']);
+        console.log('Добавлен на весы продукт ', product['name']);
     };
     Scale.prototype.getSumScale = function () {
         var amountScale = 0;
@@ -110,6 +104,7 @@ var Tomato = /** @class */ (function (_super) {
     }
     return Tomato;
 }(Products));
+/*Хранение продуктов в Array*/
 var scale1 = new Scale();
 var scalesStorageEngineArray1 = new ScalesStorageEngineArray();
 var apple1 = new Apple("Apple ranet", 100);
@@ -121,13 +116,11 @@ var tomato2 = new Tomato("Tomato akvarel", 45);
 var tomato3 = new Tomato("Tomato eldorado", 60);
 var tomato4 = new Tomato("Tomato skorospelka", 80);
 var index0 = scalesStorageEngineArray1.addItem(apple1);
-console.log(index0);
-var product1 = scalesStorageEngineArray1.getItem(index0);
-console.log(product1);
 var index1 = scalesStorageEngineArray1.addItem(apple2);
 var index2 = scalesStorageEngineArray1.addItem(apple3);
 var index3 = scalesStorageEngineArray1.addItem(apple4);
 var index4 = scalesStorageEngineArray1.addItem(tomato1);
+var product1 = scalesStorageEngineArray1.getItem(index0);
 var product2 = scalesStorageEngineArray1.getItem(index1);
 var product3 = scalesStorageEngineArray1.getItem(index2);
 var product4 = scalesStorageEngineArray1.getItem(index3);
@@ -140,34 +133,29 @@ scale1.add(product5);
 scale1.getSumScale();
 scale1.getSumScale();
 scale1.getNameList();
+/*Хранение продуктов в LocalStorage*/
 var scale2 = new Scale();
 var scalesStorageEngineLocalStorage1 = new ScalesStorageEngineLocalStorage();
 var index20 = scalesStorageEngineLocalStorage1.addItem(apple1);
-console.log(index20);
+var index21 = scalesStorageEngineLocalStorage1.addItem(apple2);
+var index22 = scalesStorageEngineLocalStorage1.addItem(apple3);
+var index23 = scalesStorageEngineLocalStorage1.addItem(apple4);
+var index24 = scalesStorageEngineLocalStorage1.addItem(tomato1);
 var product21 = scalesStorageEngineLocalStorage1.getItem(index20);
-console.log(product21);
+var product22 = scalesStorageEngineLocalStorage1.getItem(index21);
+var product23 = scalesStorageEngineLocalStorage1.getItem(index22);
+var product24 = scalesStorageEngineLocalStorage1.getItem(index23);
+var product25 = scalesStorageEngineLocalStorage1.getItem(index24);
 var apple10 = new Apple(product21['name'], product21['scale']);
+var apple20 = new Apple(product22['name'], product22['scale']);
+var apple30 = new Apple(product23['name'], product23['scale']);
+var apple40 = new Apple(product24['name'], product24['scale']);
+var apple50 = new Apple(product25['name'], product25['scale']);
 scale2.add(apple10);
-// let index21:number = scalesStorageEngineLocalStorage1.addItem(apple2);
-// let index22:any = scalesStorageEngineLocalStorage1.addItem(apple3);
-// let index23:any = scalesStorageEngineLocalStorage1.addItem(apple4);
-// let index24:any = scalesStorageEngineLocalStorage1.addItem(tomato1);
-// let product22=scalesStorageEngineLocalStorage1.getItem(index21);
-// let product23=scalesStorageEngineLocalStorage1.getItem(index22);
-// let product24=scalesStorageEngineLocalStorage1.getItem(index23);
-// let product25=scalesStorageEngineLocalStorage1.getItem(index24);   
-// let apple20:Products=new Apple(product22['name'],product21['scale']);
-// scale2.add(apple20);
-// let apple30:Products=new Apple(product23['name'],product21['scale']);
-// scale2.add(apple30);
-// let apple40:Products=new Apple(product24['name'],product21['scale']);
-// scale2.add(apple40);
-// let apple50:Products=new Apple(product25['name'],product21['scale']);
-// scale2.add(apple50);
-// scale2.add(apple20);
-// scale2.add(apple30);
-// scale2.add(apple40);
-// scale2.add(apple50);
+scale2.add(apple20);
+scale2.add(apple30);
+scale2.add(apple40);
+scale2.add(apple50);
 scale2.getSumScale();
 scale2.getSumScale();
 scale2.getNameList();
--- Scales3/app.ts
+++ Scales3/app.ts
@@ -7,10 +7,10 @@ interface IStorageEngine {
 class ScalesStorageEngineArray implements IStorageEngine{
     private arrayProducts:Array<Products>; 
     constructor(){
-        this.arrayProducts=[];
+        this.arrayProducts = [];
     }
     addItem(item:Products):number{
-        let index:number=this.arrayProducts.length;
+        let index:number = this.arrayProducts.length;
         this.arrayProducts.push(item);
         console.log('Добавлен продукт ' + item.getName());     
         return index;
@@ -22,16 +22,12 @@ class ScalesStorageEngineArray implements IStorageEngine{
         return this.arrayProducts.length;    
     };
 }
+
 class ScalesStorageEngineLocalStorage implements IStorageEngine{
-    // private index:number; 
-    // constructor(){
-    //     this.index=0;
-    // }
+    
     addItem(item:Products):number{
-        localStorage.setItem(String(localStorage.length), JSON.stringify(item));  
-        // this.index++;      
-        //console.log('Добавлен продукт ' + item['name']);  
-        return localStorage.length;      
+        localStorage.setItem(String(localStorage.length), JSON.stringify(item));          
+        return localStorage.length-1;      
     };
     getItem(index:number):Products{
         return JSON.parse(localStorage.getItem(String(index)));
@@ -49,7 +45,7 @@ class Scale <StorageEngine extends IStorageEngine> {
     
         add(product):void {           
             this.listProducts.push(product);
-            //console.log('Добавлен на весы продукт ', product['name']);
+            console.log('Добавлен на весы продукт ', product['name']);
         }
     
         getSumScale(): number {
@@ -105,76 +101,72 @@ class Scale <StorageEngine extends IStorageEngine> {
         
     }
 
-    let scale1= new Scale<ScalesStorageEngineArray>();
+    /*Хранение продуктов в Array*/
+
+    let scale1 = new Scale<ScalesStorageEngineArray>();
     let scalesStorageEngineArray1 : ScalesStorageEngineArray = new ScalesStorageEngineArray();
 
-    let apple1:Apple=new Apple("Apple ranet",100);
-    let apple2:Apple=new Apple("Apple shafran",80);
-    let apple3:Apple=new Apple("Apple antonovka",70);
-    let apple4:Apple=new Apple("Apple beliynaliv",90);
-    let tomato1:Tomato=new Tomato("Tomato cherry",50);
-    let tomato2:Tomato=new Tomato("Tomato akvarel",45);
-    let tomato3:Tomato=new Tomato("Tomato eldorado",60);
-    let tomato4:Tomato=new Tomato("Tomato skorospelka",80);     
+    let apple1:Apple = new Apple("Apple ranet",100);
+    let apple2:Apple = new Apple("Apple shafran",80);
+    let apple3:Apple = new Apple("Apple antonovka",70);
+    let apple4:Apple = new Apple("Apple beliynaliv",90);
+    let tomato1:Tomato = new Tomato("Tomato cherry",50);
+    let tomato2:Tomato = new Tomato("Tomato akvarel",45);
+    let tomato3:Tomato = new Tomato("Tomato eldorado",60);
+    let tomato4:Tomato = new Tomato("Tomato skorospelka",80);     
     
-    let index0:number = scalesStorageEngineArray1.addItem(apple1);
-    console.log(index0);
-    let product1=scalesStorageEngineArray1.getItem(index0);
-    console.log(product1);
+    let index0:number = scalesStorageEngineArray1.addItem(apple1);       
     let index1:number = scalesStorageEngineArray1.addItem(apple2);
     let index2:number = scalesStorageEngineArray1.addItem(apple3);
     let index3:number = scalesStorageEngineArray1.addItem(apple4);
     let index4:number = scalesStorageEngineArray1.addItem(tomato1);
-    let product2=scalesStorageEngineArray1.getItem(index1);
-    let product3=scalesStorageEngineArray1.getItem(index2);
-    let product4=scalesStorageEngineArray1.getItem(index3);
-    let product5=scalesStorageEngineArray1.getItem(index4);    
+
+    let product1 = scalesStorageEngineArray1.getItem(index0);
+    let product2 = scalesStorageEngineArray1.getItem(index1);
+    let product3 = scalesStorageEngineArray1.getItem(index2);
+    let product4 = scalesStorageEngineArray1.getItem(index3);
+    let product5 = scalesStorageEngineArray1.getItem(index4);    
     
     scale1.add(product1);
     scale1.add(product2);
     scale1.add(product3);
     scale1.add(product4);
     scale1.add(product5);
+
     scale1.getSumScale();
     scale1.getSumScale(); 
     scale1.getNameList();
 
 
+    /*Хранение продуктов в LocalStorage*/
 
-    let scale2= new Scale<ScalesStorageEngineArray>();
-    let scalesStorageEngineLocalStorage1 : ScalesStorageEngineLocalStorage = new ScalesStorageEngineLocalStorage();
+    let scale2 = new Scale<ScalesStorageEngineArray>();
+    let scalesStorageEngineLocalStorage1 : ScalesStorageEngineLocalStorage = new ScalesStorageEngineLocalStorage();    
     
-    
-    let index20:number = scalesStorageEngineLocalStorage1.addItem(apple1);
-    console.log(index20);
-    let product21=scalesStorageEngineLocalStorage1.getItem(index20);
-    console.log(product21);
-    let apple10:Apple=new Apple(product21['name'],product21['scale']);
-    
+    let index20:number = scalesStorageEngineLocalStorage1.addItem(apple1);    
+    let index21:number = scalesStorageEngineLocalStorage1.addItem(apple2);
+    let index22:number = scalesStorageEngineLocalStorage1.addItem(apple3);
+    let index23:number = scalesStorageEngineLocalStorage1.addItem(apple4);
+    let index24:number = scalesStorageEngineLocalStorage1.addItem(tomato1);
+
+    let product21 = scalesStorageEngineLocalStorage1.getItem(index20);
+    let product22 = scalesStorageEngineLocalStorage1.getItem(index21);
+    let product23 = scalesStorageEngineLocalStorage1.getItem(index22);
+    let product24 = scalesStorageEngineLocalStorage1.getItem(index23);
+    let product25 = scalesStorageEngineLocalStorage1.getItem(index24);   
+
+    let apple10:Apple = new Apple(product21['name'],product21['scale']);
+    let apple20:Apple = new Apple(product22['name'],product22['scale']);    
+    let apple30:Apple = new Apple(product23['name'],product23['scale']);    
+    let apple40:Apple = new Apple(product24['name'],product24['scale']);    
+    let apple50:Apple = new Apple(product25['name'],product25['scale']);
+
     scale2.add(apple10);
-    // let index21:number = scalesStorageEngineLocalStorage1.addItem(apple2);
-    // let index22:any = scalesStorageEngineLocalStorage1.addItem(apple3);
-    // let index23:any = scalesStorageEngineLocalStorage1.addItem(apple4);
-    // let index24:any = scalesStorageEngineLocalStorage1.addItem(tomato1);
-    // let product22=scalesStorageEngineLocalStorage1.getItem(index21);
-    // let product23=scalesStorageEngineLocalStorage1.getItem(index22);
-    // let product24=scalesStorageEngineLocalStorage1.getItem(index23);
-    // let product25=scalesStorageEngineLocalStorage1.getItem(index24);   
+    scale2.add(apple20);
+    scale2.add(apple30);
+    scale2.add(apple40);
+    scale2.add(apple50);    
     
-    // let apple20:Products=new Apple(product22['name'],product21['scale']);
-    // scale2.add(apple20);
-    // let apple30:Products=new Apple(product23['name'],product21['scale']);
-    // scale2.add(apple30);
-    // let apple40:Products=new Apple(product24['name'],product21['scale']);
-    // scale2.add(apple40);
-    // let apple50:Products=new Apple(product25['name'],product21['scale']);
-    // scale2.add(apple50);
-    
-    
-    // scale2.add(apple20);
-    // scale2.add(apple30);
-    // scale2.add(apple40);
-    // scale2.add(apple50);
     scale2.getSumScale();
     scale2.getSumScale(); 
     scale2.getNameList();
